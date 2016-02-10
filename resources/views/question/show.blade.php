99167fbfc12c36c13ef3548e2c05b30b299e5c7b
Manoj Byanjankar <manoj.byanjankar@yipl.com.np>
2016-02-10 11:02:56 +0545

    question descripition

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -24,7 +24,7 @@
                                             @endif
                                         @endforeach
                                     @else
-                                        {{$metadata }}
+                                        {!!$metadata !!}
                                     @endif
                                 </td>
                             </tr>
