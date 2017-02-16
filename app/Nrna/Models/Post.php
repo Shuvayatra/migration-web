f4ef1c8143db0f899ad7d5f23f12f2e4dd38a9b0
Manoj Byanjankar <manoj.byanjankar@yipl.com.np>
2017-02-16 08:58:56 +0545

    post audit url bug fix

--- app/Nrna/Models/Post.php
+++ app/Nrna/Models/Post.php
@@ -142,7 +142,7 @@ class Post extends Model
 
         if (!empty($metadata->data->audio)) {
 
-            $metadata->data->audio = sprintf('%s/%s', url(Self::UPLOAD_PATH), $metadata->data->audio);
+            $metadata->data->audio = sprintf('%s/%s', url(Self::UPLOAD_PATH), (string)$metadata->data->audio);
 
             if (isset($metadata->data->thumbnail)) {
                 $metadata->data->thumbnail = sprintf('%s/%s', url(Self::UPLOAD_PATH), $metadata->data->thumbnail);
