4a75cc4573bf44220b799699d8877c89150c99ba
ashimregmi <aregmi@hamropatro.com>
2017-11-29 14:00:53 +0545

    changed rss feed post ordering parameter to created_at

--- app/Nrna/Repositories/Post/PostRepository.php
+++ app/Nrna/Repositories/Post/PostRepository.php
@@ -356,7 +356,7 @@ class PostRepository implements PostRepositoryInterface
         }
 
         $query->from($this->db->raw($from));
-        $query->orderBy('updated_at', 'DESC');
+        $query->orderBy('created_at', 'DESC');
         $query->published();
 
         return $query->paginate();
